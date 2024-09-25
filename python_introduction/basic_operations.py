bce061a5f8e868476297f7cea4c927ddf3e50b6c
AsmaaEissa <82784950+Asmaatef@users.noreply.github.com>
2024-09-25 13:36:15 +0200

    updaded the directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 =10
+number2 =5
+sum =number1 + number2
+negative =number1 - number2
+multiplication =number1 * number2
+print(sum)
+print(negative)
+print(multiplication)
+  # try to upladed
+  #  
