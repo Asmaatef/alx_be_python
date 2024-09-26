875ab614db905dea93dd070f91489b55dacda39d
AsmaaEissa <82784950+Asmaatef@users.noreply.github.com>
2024-09-26 11:54:07 +0200

    uploaded simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+
+
+def simple_interest (P ,R ,T):
+     simpleinterest = ( P * R * T)
+     return simpleinterest
+P = 1000
+R =  0.05          
+T = 3
+print('simple interest :{0}'.format (simple_interest(P,R,T)))
