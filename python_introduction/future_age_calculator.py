433ca72e2a1ffc48aae9afb15cdc26bce7e98ef8
AsmaaEissa <82784950+Asmaatef@users.noreply.github.com>
2024-09-26 13:22:11 +0200

    Update future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,5 @@
 question = "How old are you? "
-x =print(27)
-y = 27 + 27
-print('In 2050, you will be' ,y , 'years old' )
+x =print(question)
+y = 27 + question
+print('In 2050, you will be' , y , 'years old' )
 
