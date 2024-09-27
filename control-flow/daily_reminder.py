44839392b45ce2c900a93956227d9835fc0dede0
AsmaaEissa <82784950+Asmaatef@users.noreply.github.com>
2024-09-27 14:20:15 +0200

    finish the reminder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,4 @@
+project = str(input("Enter your task:"))
+priority = str (input("Priority (high/medium/low)"))
+time = str (input("Is it time-bound? (yes/no)"))
+print ("Reminder: " ,project ,"is a", priority ,"priority task that requires immediate attention today!")
