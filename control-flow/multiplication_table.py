847221a6272e288092512cd1fb8be5a2e5055aca
AsmaaEissa <82784950+Asmaatef@users.noreply.github.com>
2024-09-27 14:01:51 +0200

    done the multipliation table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,13 @@
+
+x = int(input("Enter the number for the multiplication table: "))
+y = int(input("Enter the number of rows for the multiplication table: "))
+
+for i in range(1, y + 1):
+    result = x * i
+    print(f"{x} * {i} = {result}")
+    if i >= 20:
+        print("The number of rows has been limited to 20.")
+        break
+else:
+    if y > 20:
+        print("The number of rows has been limited to 20.")
