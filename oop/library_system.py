e6a8d009f42fa40a3a6874fa1c8c385a708d19ce
AsmaaEissa <82784950+Asmaatef@users.noreply.github.com>
2024-10-13 21:59:37 +0200

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,13 +4,13 @@ class Book :
         self.author = author
     def __str__(self):
         return f" Book : {self.title} by {self.author}"
-class EBook (Book) :
+class EBook(Book) :
     def __init__(self ,title ,author ,file_size):
         super.__init__(title ,author)
         self.file_size = file_size
     def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB" 
-class PrintBook (Book) :
+class PrintBook(Book) :
     def __init__(self, title, author ,page_count):
         super().__init__(title, author)
         self.__init__(title , author ,page_count)
